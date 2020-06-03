d83662b94a087218972066828529c87194e5dc78
dot <shuhei.kondo@gmail.com>
2020-06-03 23:12:18 +0900

    Updates

--- src/gatsby-theme-blog/components/bio-content.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from "react"
-import { Styled } from "theme-ui"
-
-export default () => (
-  <Fragment>
-    Words by <Styled.a href="https://dot.codes/">dot</Styled.a>.
-    <br />
-  </Fragment>
-)
